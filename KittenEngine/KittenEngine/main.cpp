539cf4a2b72496ed3ae1ca75c5ceff1ef79bca52
Jerry <jerry060599@gmail.com>
2021-09-14 19:29:52 -0600

    Remove all dependencies that can be obtained from vcpkg

--- KittenEngine/KittenEngine/main.cpp
+++ KittenEngine/KittenEngine/main.cpp
@@ -5,9 +5,9 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_glfw.h"
-#include "imgui/imgui_impl_opengl3.h"
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
 #include "KittenEngine/includes/KittenEngine.h"
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 KittenEngine
 
 vcpkg dependencies:
-assimp, eigen3, stb headers
+assimp, eigen3, stb headers, glad, glfw, imgui[opengl3-glad-binding], glm
