a46f8d7dade47410a2890a7dc133b8ea508837a1
Jerry <jerry060599@gmail.com>
2022-01-09 22:34:56 -0700

    New orthoBasis function

--- KittenEngine/KittenEngine/KittenEngine/includes/modules/Common.h
+++ KittenEngine/KittenEngine/KittenEngine/includes/modules/Common.h
@@ -173,12 +173,44 @@ inline vec3 safeOrthonorm(vec3 v) {
 	return normalize(cross(vec3(0, 1, 0), v));
 }
 
+inline mat3 orthoBasisZ(vec3 zNorm) {
+	mat3 basis;
+	basis[2] = zNorm;
+	if (zNorm.z >= zNorm.y) {
+		const float a = 1.0f / (1.0f + zNorm.z);
+		const float b = -zNorm.x * zNorm.y * a;
+		basis[0] = vec3(1.0f - zNorm.x * zNorm.x * a, b, -zNorm.x);
+		basis[1] = vec3(b, 1.0f - zNorm.y * zNorm.y * a, -zNorm.y);
+	}
+	else {
+		const float a = 1.0f / (1.0f + zNorm.y);
+		const float b = -zNorm.x * zNorm.z * a;
+		basis[0] = vec3(b, -zNorm.z, 1.0f - zNorm.z * zNorm.z * a);
+		basis[1] = vec3(1.0f - zNorm.x * zNorm.x * a, -zNorm.x, b);
+	}
+	return basis;
+}
+
+inline mat3 orthoBasisY(vec3 yNorm) {
+	mat3 basis;
+	basis[1] = yNorm;
+	if (yNorm.z >= yNorm.y) {
+		const float a = 1.0f / (1.0f + yNorm.z);
+		const float b = -yNorm.x * yNorm.y * a;
+		basis[0] = vec3(1.0f - yNorm.x * yNorm.x * a, b, -yNorm.x);
+		basis[2] = vec3(b, 1.0f - yNorm.y * yNorm.y * a, -yNorm.y);
+	}
+	else {
+		const float a = 1.0f / (1.0f + yNorm.y);
+		const float b = -yNorm.x * yNorm.z * a;
+		basis[0] = vec3(b, -yNorm.z, 1.0f - yNorm.z * yNorm.z * a);
+		basis[2] = vec3(1.0f - yNorm.x * yNorm.x * a, -yNorm.x, b);
+	}
+	return basis;
+}
+
 inline mat4 rotateView(vec3 dir) {
-	mat4 o(1);
-	o[2] = vec4(-normalize(dir), 0.f);
-	o[0] = vec4(safeOrthonorm(vec3(o[2])), 0.f);
-	o[1] = vec4(cross(vec3(o[2]), vec3(o[0])), 0.f);
-	return transpose(o);
+	return mat4(orthoBasisZ(normalize(dir)));
 }
 
 inline int numBatches(int n, int batchSize) {
