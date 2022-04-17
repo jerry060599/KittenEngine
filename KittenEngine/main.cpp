49ab5b03a378bbaebec4d3fb407061f9dbfd9ce8
Jerry <jerry060599@gmail.com>
2022-04-16 20:45:07 -0600

    removed unnecessary includes

--- KittenEngine/main.cpp
+++ KittenEngine/main.cpp
@@ -1,14 +1,3 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
-
 #include "KittenEngine/includes/KittenEngine.h"
 #include "KittenEngine/includes/modules/glTempVar.h"
 
